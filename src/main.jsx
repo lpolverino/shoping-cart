dd5fef4dee81402eea379d0f2a912c436c7c5934
lpolverino <lorenz.polverino@gmail.com>
2023-12-26 11:40:55 -0300

    added routing points

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./componets/App/App"
 import './index.css'
+import Shop from './componets/Shop/Shop'
+import ErrorPage from "./componets/ErrorPage/ErrorPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "shop",
+    element: <Shop />,
+  },
+]);
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
